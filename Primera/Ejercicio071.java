17b5e0da3617e86207bad75b07c9233b82d5d86b
el12pachi <114700628+el12pachi@users.noreply.github.com>
2024-11-06 08:14:09 +0100

    Update Ejercicio071.java

--- Primera/Ejercicio071.java
+++ Primera/Ejercicio071.java
@@ -4,10 +4,10 @@ package Primera;
 public class Ejercicio071 {
 
     public static void main(String arg[]) {
-        char frase1[] = { 'E', 'n', ' ', 'u', 'n', ' ', 'l', 'u', 'g', 'a', 'r', ' ', 'd', 'e', ' ', 'l', 'a', ' ', 'M',
+        char frase1[] = { 'e', 'n', ' ', 'u', 'n', ' ', 'l', 'u', 'g', 'a', 'r', ' ', 'd', 'e', ' ', 'l', 'a', ' ', 'M',
                 'a', 'n', 'c', 'h', 'a' };
-        char sustituto[] = { 'l', 'a' };
-        char sustituir[] = { 'X', 'N', 'M', 'R', 'S' };
+        char sustituto[] = { 'e', 'n' };
+        char sustituir[] = { 'X', 'X', 'X', 'X', 'X' };
         char frase2[] = new char[150];
         for (int i = 0, j = 0, aux = 0; i < frase1.length; i++, j++, aux = 0) {
             if (sustituto[0] == frase1[i]) {
