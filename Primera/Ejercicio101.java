b0e0fbea695d828919f5b2f70f7aad92262a7063
el12pachi <114700628+el12pachi@users.noreply.github.com>
2024-11-27 20:44:35 +0100

    Update Ejercicio101.java

--- Primera/Ejercicio101.java
+++ Primera/Ejercicio101.java
@@ -1,65 +1,75 @@
 package Primera;
 
 public class Ejercicio101 {
-    public static void character(){
-        char frase[] = { 'h', 'o', 'l', 'a', ' ', 'm', 'u', 'n', 'd', 'o', ' ','q','u','e',' ','t','a','l',' ','e','s','t','a','n',' ','t','o','d','o','s',' ' };
+    public static void character() {
+        char frase[] = { 'h', 'o', 'l', 'a', ' ', 'm', 'u', 'n', 'd', 'o', ' ', 'q', 'u', 'e', ' ', 't', 'a', 'l', ' ',
+                'e', 's', 't', 'a', 'n', ' ', 't', 'o', 'd', 'o', 's', ' ' };
         int inicio = 0;
         int fin = 0;
-        int contador = 0;
+        boolean contador = true;
         int contadorVocales = 0;
         while (fin < frase.length) {
-            contador = 0;
+            contador = false;
             while (frase[fin] != ' ') {
                 fin++;
-            } 
+            }
             for (int x = inicio; x < fin; x++) {
                 switch (frase[x]) {
-                    case 'a':case 'e':case 'i':case 'o':case 'u':
-                        contador ++;
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        contador = true;
                         break;
-                
+
                     default:
                         break;
                 }
             }
-            inicio =  ++fin;
-            if (contador > 0) {
+            inicio = ++fin;
+            if (contador) {
                 contadorVocales++;
             }
         }
         System.out.printf("El numero de palabras que tienen vocales es %d \n", contadorVocales);
     }
 
-    public static void string(){
+    public static void string() {
         String frase = "hola mundo que tal estan todos ";
-        int contador = 0;
+        boolean contador = true;
         int contadorVocales = 0;
         int inicio = 0;
         int fin = frase.indexOf(" ");
-        while (fin != -1 ){
-            contador = 0;
+        while (fin != -1) {
+            contador = false;
             for (int x = inicio; x < fin; x++) {
                 switch (frase.charAt(x)) {
-                    case 'a':case 'e':case 'i':case 'o':case 'u':
-                        contador ++;
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        contador = true;
                         break;
-                
+
                     default:
                         break;
                 }
             }
-            inicio =  ++fin;
+            inicio = ++fin;
             fin = frase.indexOf(" ", fin);
-            if (contador > 0) {
+            if (contador) {
                 contadorVocales++;
             }
         }
         System.out.printf("El numero de palabras que tienen vocales es %d \n", contadorVocales);
 
     }
+
     public static void main(String[] args) {
         string();
         character();
-        
+
     }
 }
