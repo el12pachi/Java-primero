3261924c51c15e8620e7cc9e025a464ad77b6908
el12pachi <114700628+el12pachi@users.noreply.github.com>
2024-12-05 10:50:27 +0100

    Update

---
+++ Segunda/Ejercicio01/Casa.java
@@ -0,0 +1,59 @@
+package Segunda.Ejercicio01;
+
+public class Casa {
+    private String direccion;
+    private double numMetros;
+    private String material;
+    private int numHabitaciones;
+
+    public Casa(String direccion, double numMetros, String material, int numHabitaciones) {
+        this.direccion = direccion;
+        this.numMetros = numMetros;
+        this.material = material;
+        this.numHabitaciones = numHabitaciones;
+    }
+
+    public Casa(String direccion, double numMetros, String material) {
+        this.direccion = direccion;
+        this.numMetros = numMetros;
+        this.material = material;
+        this.numHabitaciones = 1;
+    }
+    public void getCaraterisicas() {
+        System.out.printf("%s numero de metros %.2f esta construida con %s y tiene %d habitaciones. \n",this.direccion, this.numMetros,this.material, this.numHabitaciones);
+    }
+    
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setNumMetros(double numMetros) {
+        this.numMetros = numMetros;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public void setNumHabitaciones(int numHabitaciones) {
+        this.numHabitaciones = numHabitaciones;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public double getNumMetros() {
+        return numMetros;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public int getNumHabitaciones() {
+        return numHabitaciones;
+    }
+
+
+}
---
+++ Segunda/Ejercicio01/Ejecutable.java
@@ -0,0 +1,18 @@
+package Segunda.Ejercicio01;
+
+public class Ejecutable {
+    public static void main(String[] args) {
+        Casa casa1 = new Casa("C/. María Auxiliadora nº59 " , 1200, "Ladrillo");
+        casa1.getCaraterisicas();
+        
+        // Casa hola[] = new Casa[10];
+
+        // for (int x = 0; x < 10; x++) {
+        //     Casa n = new Casa("C/. María Auxiliadora nº59 " + x, x, "Ladrillo" +x);
+        //     hola[x] = n;
+        // }
+        // for(Casa x:hola){
+        //     System.out.println(x.getDireccion());
+        // }
+    }
+}
